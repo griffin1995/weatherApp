2d9752e38ad874fd9a55ebe22a991a958b7db276
griffin1995 <jtgriffin95@gmail.com>
2024-03-21 21:21:40 +0000

    normal api

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,6 @@
-//import { env } from "process"
-// const APIKEY = "65ad34ef51cc68f1567d459fc99efa63" // base
+import { env } from "process"
+const APIKEY = "65ad34ef51cc68f1567d459fc99efa63" // base
 
-const APIKEY = '664a5c83e36aa6a36f36ff14c3f3b383'; // my
 
 export const apiUrl = (location: string) => {
 	return `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIKEY}`;
